347a3b71e90746647bce952f857992ff72f76326
Blessed Sode <emi.square23@gmail.com>
2025-07-28 22:12:39 +0100

    updated

--- fetch-data.js
+++ fetch-data.js
@@ -1,4 +1,4 @@
-async function fetchUserDAta(){
+async function fetchUserData(){
 
     const apiUrl = 'https://jsonplaceholder.typicode.com/users'
     const dataContainer = document.getElementById('api-data')
