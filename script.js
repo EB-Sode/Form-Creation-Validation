cee726c10485675a270e239e1272d06e6ed15e56
Blessed Sode <emi.square23@gmail.com>
2025-07-28 20:59:05 +0100

    updated

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
             messages.push("Your password is not long enough")
         }
 
-        feedbackDiv.style.display = block;
+        feedbackDiv.style.display = 'block';
 
         if(isValid){
             feedbackDiv.textContent = "Registration successful!";
